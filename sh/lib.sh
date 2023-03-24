19116ed3b624f213a0b48b02e00d277647e975b8
Jon Jagger <jon@jaggersoft.com>
2023-03-24 12:18:14 +0000

    Tidy bash scripts

--- sh/lib.sh
+++ sh/lib.sh
@@ -52,15 +52,15 @@ on_ci_publish_tagged_image()
 {
   if ! on_ci; then
     echo 'not on CI so not publishing tagged image'
-    return
+  else
+    echo 'on CI so publishing tagged image'
+    local -r image="$(image_name)"
+    local -r tag="$(image_tag)"
+    echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
+    docker push ${image}:latest
+    docker push ${image}:${tag}
+    docker logout
   fi
-  echo 'on CI so publishing tagged image'
-  local -r image="$(image_name)"
-  local -r tag="$(image_tag)"
-  echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
-  docker push ${image}:latest
-  docker push ${image}:${tag}
-  docker logout
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
